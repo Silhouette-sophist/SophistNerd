998f0683032edf802edb089dad18b2427c94418d
SophistNerd <C_jin928@163.com>
2022-02-27 11:07:43 +0800

    [feature] 更换应用图标，请求图片增加随机性

--- app/src/main/java/com/example/sophistnerd/api/UnsplashApi.kt
+++ app/src/main/java/com/example/sophistnerd/api/UnsplashApi.kt
@@ -3,6 +3,7 @@ package com.example.sophistnerd.api
 import com.example.sophistnerd.data.UnsplashResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
+import kotlin.random.Random
 
 interface UnsplashApi {
 
@@ -10,8 +11,8 @@ interface UnsplashApi {
     suspend fun searchPhotos(
         @Query("query") query: String,
         @Query("page") page: Int = 1,
-        @Query("per_page") perPage: Int = 10,
-        @Query("order_by") orderBy: String = "relevant", //latest
+        @Query("per_page") perPage: Int = RANDOM_INT.nextInt(10, 50),
+        @Query("order_by") orderBy: String = if(RANDOM_INT.nextBoolean()) "latest" else "relevant",
         @Query("orientation") orientation: String = "landscape", //landscape, portrait, squarish
         @Query("client_id") clientId: String = "DaMXc7TjqqKRlN_pK95laDMWOX_gfdTJk7cSax26NBE"
     ): UnsplashResponse
@@ -25,5 +26,6 @@ interface UnsplashApi {
 
     companion object {
         const val BASE_URL = "https://api.unsplash.com/"
+        val RANDOM_INT = Random(50)
     }
 }
