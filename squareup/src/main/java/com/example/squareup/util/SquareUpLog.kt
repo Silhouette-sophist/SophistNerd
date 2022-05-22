2a85161a5286a07994b0a21ed0659b98b4a0e714
SophistNerd <C_jin928@163.com>
2022-05-22 17:53:02 +0800

    [feature] 增加日志tag

--- squareup/src/main/java/com/example/squareup/util/SquareUpLog.kt
+++ squareup/src/main/java/com/example/squareup/util/SquareUpLog.kt
@@ -8,10 +8,31 @@ object SquareUpLog {
     val logger = Logger.getLogger("SquareUpLog")
 
     fun info(msg : String) {
-        logger.info(msg)
+        val loggerTag = getLoggerTag()
+        logger.info("[$loggerTag] $msg")
     }
 
     fun error(throwable: Throwable) {
         logger.warning("${throwable.message}, ${throwable.cause}, ${Arrays.toString(throwable.stackTrace)}")
+    }
+
+    private fun getLoggerTag() : String {
+        val stackTrace = Thread.currentThread().stackTrace
+        val stackTraceElement = stackTrace[stackTrace.size - 2]
+        val methodName = stackTraceElement.methodName
+        val simpleClassName = getSimpleClassName(stackTraceElement.className.split("$")[0])
+        return "[$simpleClassName $methodName]"
+    }
+
+    private fun getSimpleClassName(className: String) : String{
+        var simpleClassName = ""
+        val split = className.split("$")
+        if (split.isNotEmpty()) {
+            val classSplit = split[0].split(".")
+            if(classSplit.isNotEmpty()) {
+                simpleClassName = classSplit[classSplit.size - 1]
+            }
+        }
+        return simpleClassName
     }
 }
